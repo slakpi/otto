3b56e01615202a24bdaa2328a5aea2e7cfd51846
Randy Widell <randy@slakr.org>
2016-01-11 18:17:34 +0000

    Moved otto source files to trunk.
    git-svn-id: http://shadow.home.slakr.net:32020/svn/randy/slakrdotorg/otto/trunk@874 65f2e670-5c5c-4068-a38c-2475aa18ad08

---
+++ AveragingBuffer.cpp
@@ -0,0 +1,38 @@
+#include <stdexcept>
+#include "AveragingBuffer.hpp"
+#include "Utilities.hpp"
+
+AveragingBuffer::AveragingBuffer(unsigned int _bufferLen)
+:	i(0),
+	samples(0),
+	bufferLen(_bufferLen),
+	total(0)
+{
+	if (bufferLen < 1)
+		throw std::invalid_argument("_bufferLen");
+	
+	buffer.resize(bufferLen);
+	memset(buffer.data(), 0, sizeof(double) * bufferLen);
+}
+	
+double AveragingBuffer::pushSample(double _sample)
+{
+	total -= buffer[i];
+	total += _sample;
+	buffer[i] = _sample;
+	i = (i + 1) % bufferLen;
+	samples = min(bufferLen, samples + 1);
+	return total / samples;
+}
+
+double AveragingBuffer::average() const
+{
+	return (samples > 0 ? total / samples : 0);
+}
+	
+void AveragingBuffer::reset()
+{
+	i = samples = 0;
+	total = 0;
+	memset(buffer.data(), 0, sizeof(double) * bufferLen);
+}
