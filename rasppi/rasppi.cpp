6d8508a978ecc7b998fe6553a3f0f314a049c118
Randy Widell <randy@slakr.org>
2016-01-31 19:40:26 +0000

    Modified the rasppi Makefile to include the GISDatabase code.  Started the rasppi daemon code.
    git-svn-id: http://shadow.home.slakr.net:32020/svn/randy/slakrdotorg/otto/trunk@895 65f2e670-5c5c-4068-a38c-2475aa18ad08

--- rasppi/rasppi.cpp
+++ rasppi/rasppi.cpp
@@ -1,4 +1,79 @@
-int main()
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <cstdio>
+#include <cstdlib>
+#include <fcntl.h>
+#include <cerrno>
+#include <unistd.h>
+#include <syslog.h>
+#include <cstring>
+#include <csignal>
+#include <cstdarg>
+
+static int running = 1;
+
+static void signalHandler(int _signum)
 {
+	switch (_signum)
+	{
+	case SIGINT:
+	case SIGTERM:
+		running = 0;
+		break;
+	}
+}
+
+static void logCallback(const char *_fmt, ...)
+{
+	int len;
+	va_list args;
+	char *str;
+
+	va_start(args, _fmt);
+	len = vsnprintf(nullptr, 0, _fmt, args);
+
+	if (len < 1)
+		return;
+
+	va_start(args, _fmt);
+	str = new char[len + 1];
+	vsnprintf(str, len + 1, _fmt, args);
+	syslog(LOG_INFO, str);
+	delete [] str;
+}
+
+int main(int _argc, char* _argv[])
+{
+	pid_t pid, sid;
+	int loop = 0;
+
+	signal(SIGINT, signalHandler);
+	signal(SIGTERM, signalHandler);
+
+	openlog("otto", LOG_NDELAY | LOG_PID, LOG_USER);
+	syslog(LOG_INFO, "OTTO daemon starting...");
+
+	pid = fork();
+	if (pid < 0)	return -1;
+	if (pid > 0)	return 0;
+
+	umask(0);
+
+	sid = setsid();
+	if (sid < 0)
+		return -1;
+
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+
+	while (running != 0)
+	{
+		logCallback("OTTO loop number %d", ++loop);
+		sleep(5);
+	}
+
+	closelog();
+
 	return 0;
 }
