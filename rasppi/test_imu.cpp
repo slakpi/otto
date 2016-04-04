2af50293a0d636f3c85ce772ba7fe221aa37145d
Randy Widell <randy@slakr.org>
2016-04-04 17:21:36 +0000

    Added Madgwick and Mahony AHRS filter files.  Began experimenting with them in the IMU test.
    git-svn-id: http://shadow.home.slakr.net:32020/svn/randy/slakrdotorg/otto/trunk@926 65f2e670-5c5c-4068-a38c-2475aa18ad08

--- rasppi/test_imu.cpp
+++ rasppi/test_imu.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 #include <csignal>
 #include <ctime>
+#include <cmath>
 #include <unistd.h>
 #include <wiringPi.h>
 #include <AveragingBuffer.hpp>
 #include "LSM6DS33.hpp"
 #include "LIS3MDL.hpp"
 #include "HD44780.hpp"
+#include "Mahony_AHRS.h"
 
 #define DELAY 20000
 
@@ -26,18 +28,29 @@ static void signalHandler(int _signal)
 	}
 }
 
+#define RAD2DEGF(_r) ((float)((_r) * 180.0f / M_PI))
+
+void quaternionToEuler(float q[4], float e[3])
+{
+   float sqw = q[0] * q[0];
+   float sqx = q[1] * q[1];
+   float sqy = q[2] * q[2];
+   float sqz = q[3] * q[3];
+   e[0] = atan2f(2.f * (q[1]*q[2] + q[3]*q[0]), sqx - sqy - sqz + sqw);
+   e[1] = asinf(-2.f * (q[1]*q[3] - q[2]*q[0]));
+   e[2] = atan2f(2.f * (q[2]*q[3] + q[1]*q[0]), -sqx - sqy + sqz + sqw);
+}
+
 int main(int _argc, char* _argv[])
 {
 	LIS3MDL imuMag;
 	LSM6DS33 imuGyroAccel;
-	Vector<double> m, a, aa, g, ga;
-	AveragingBuffer gx(3), gy(3), gz(3);
-	AveragingBuffer ax(3), ay(3), az(3);
-	double hdg;
+	HD44780 lcd;
+	Vector<double> m, a, g;
 	int64_t t, r;
 	timespec spec;
-	HD44780 lcd;
 	char buf[17];
+	float q[4], e[3];
 
 	if (wiringPiSetup() == -1)
 	{
@@ -70,7 +83,7 @@ int main(int _argc, char* _argv[])
 	r = spec.tv_sec * 1000000LL;
 	r += spec.tv_nsec / 1000LL;
 
-	cout << "t,Mx,My,Mz,Hdg,Ax,Ay,Az,Axa,Aya,Aza,Gx,Gy,Gz,Gxa,Gya,Gyz\n";
+	cout << "t,Mx,My,Mz,Ax,Ay,Az,Gx,Gy,Gz,P,R,Y\n";
 
 	while (run)
 	{
@@ -79,26 +92,28 @@ int main(int _argc, char* _argv[])
 		t += spec.tv_nsec / 1000LL;
 		t -= r;
 
-		imuMag.readMag(m, hdg);
+		imuMag.readMag(m);
 		imuGyroAccel.readGyro(g);
 		imuGyroAccel.readAccel(a);
 
-		aa.x = a.x - ax.pushSample(a.x);
-		aa.y = a.y - ay.pushSample(a.y);
-		aa.z = a.z - az.pushSample(a.z);
+		MahonyAHRSupdate(g.x, g.y, g.z, a.x, a.y, a.z, m.x, m.y, m.z);
+		q[0] = q0;
+		q[1] = q1;
+		q[2] = q2;
+		q[3] = q3;
+		quaternionToEuler(q, e);
 
-		ga.x = g.x - gx.pushSample(g.x);
-		ga.y = g.y - gy.pushSample(g.y);
-		ga.z = g.z - gz.pushSample(g.z);
+		cout << m.x << "," << m.y << "," << m.z << "," << a.x << "," <<
+			a.y << "," << a.z << "," << g.x << "," << g.y << "," <<
+			g.z << "," << RAD2DEGF(e[0]) << "," << RAD2DEGF(e[1]) << "," <<
+			RAD2DEGF(e[2]) << endl;
 
-		cout << t << "," << m.x << "," << m.y << "," << m.z << "," <<
-			hdg << "," << a.x << "," << a.y << "," << a.z << "," <<
-			aa.x << "," << aa.y << "," << aa.z << "," << g.x << "," <<
-			g.y << "," << g.z << "," << ga.x << "," << ga.y << "," <<
-			ga.z << endl;
-
-		snprintf(buf, 17, "M%3d", (int)hdg);
+		snprintf(buf, 17, "%6.1f %6.1f", RAD2DEGF(e[0]), RAD2DEGF(e[1]));
 		lcd.setCursorPos(0, 0);
+		lcd.writeString(buf);
+
+		snprintf(buf, 17, "%6.1f", RAD2DEGF(e[2]));
+		lcd.setCursorPos(1, 0);
 		lcd.writeString(buf);
 
 		usleep(DELAY);
